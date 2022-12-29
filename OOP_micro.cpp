fd1bd15e6008c5434e31d4a112a606c0cc6cfc14
Jeevan-Gharate <59790218+Jeevan-Gharate@users.noreply.github.com>
2022-12-30 05:23:32 +0530

    final edits

--- OOP_micro.cpp
+++ OOP_micro.cpp
@@ -6,12 +6,20 @@
 #include <conio.h>
 #include <io.h>
 #include <chrono>
+#include <sstream>
+#include <iomanip>
 #include <ctime>
 #define SIZE 1000
 using namespace std;
  
 const string ADMIN_USERNAME = "admin";
 string LoggedInUser;
+
+void pause_screen()
+{
+	cout<<"\nPress enter to continue\n";
+	getch();
+}
 
 void clear_screen()
 {
@@ -44,46 +52,34 @@ bool ChCrDir() {
         return true;
     #endif
     }
-
 }
 
 
 string getLimitTime() {
     // Get the current time
     auto now = std::chrono::system_clock::now();
-
-    // Add 30 minutes to the time
-    auto time_plus_30_minutes = now + std::chrono::duration_cast<std::chrono::system_clock::duration>(std::chrono::minutes(30));
-
+    // Add 10 minutes to the time
+    auto time_plus_10_minutes = now + std::chrono::duration_cast<std::chrono::system_clock::duration>(std::chrono::minutes(10));
     // Convert the time to a timestamp
-    std::time_t timestamp = std::chrono::system_clock::to_time_t(time_plus_30_minutes);
-
+    std::time_t timestamp = std::chrono::system_clock::to_time_t(time_plus_10_minutes);
     // Convert the timestamp to a string
     std::string time_str = std::ctime(&timestamp);
-
     // Print the time string
     std::cout << "Unlock Time: " << time_str << std::endl;
-
     return time_str;
 }
-
-
 
 
 char* enc_b64(string input_str, int len_str)
 {
    
     char char_set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-      
-    char *res_str = (char *) malloc(SIZE * sizeof(char));
-      
+    char *res_str = (char *) malloc(SIZE * sizeof(char)); 
     int index, no_of_bits = 0, padding = 0, val = 0, count = 0, temp;
-    int i, j, k = 0;
-      
+    int i, j, k = 0;   
     for (i = 0; i < len_str; i += 3)
         {
             val = 0, count = 0, no_of_bits = 0;
-  
             for (j = i; j < len_str && j <= i + 2; j++)
             {
 
@@ -96,10 +92,7 @@ char* enc_b64(string input_str, int len_str)
             }
   
             no_of_bits = count * 8;
-  
             padding = no_of_bits % 3;
-  
-
             while (no_of_bits != 0)
             {
                 if (no_of_bits >= 6)
@@ -119,14 +112,11 @@ char* enc_b64(string input_str, int len_str)
                 res_str[k++] = char_set[index];
             }
     }
-
     for (i = 1; i <= padding; i++)
     {
         res_str[k++] = '=';
     }
-  
     res_str[k] = '\0';
-  
     return res_str;
   
 }
@@ -137,14 +127,9 @@ char* dc_b64(string encoded, int len_str)
     char* decoded_string;
  
     decoded_string = (char*)malloc(sizeof(char) * SIZE);
- 
     int i, j, k = 0;
- 
     int num = 0;
- 
-
     int count_bits = 0;
- 
     for (i = 0; i < len_str; i += 4)
     {
         num = 0, count_bits = 0;
@@ -177,17 +162,13 @@ char* dc_b64(string encoded, int len_str)
                 count_bits -= 2;
             }
         }
- 
         while (count_bits != 0)
         {
             count_bits -= 8;
-
             decoded_string[k++] = (num >> count_bits) & 255;
         }
     }
-
-    decoded_string[k] = '\0';
- 
+    decoded_string[k] = '\0'; 
     return decoded_string;
 }
 
@@ -243,16 +224,16 @@ int adminFunctions(){
             DWORD file_attributes = GetFileAttributes((dir_path + temp_un).c_str());
             if (file_attributes == INVALID_FILE_ATTRIBUTES) {
                 cout << "The User '" << temp_un << "' does not exist!" << endl;
-                Sleep(3000);
+                pause_screen();
                 adminFunctions();
             } else{
                     if (DeleteFile((dir_path + temp_un).c_str())) {
                         cout << "The user '" << temp_un << "' was successfully deleted" << endl;
-                        Sleep(3000);
+                        pause_screen();
                         adminFunctions();
                     } else {
                         cerr << "Error: The user '" << temp_un << "' could not be deleted" << endl;
-                        Sleep(3000);
+                        pause_screen();
                         adminFunctions();
                     }
             }
@@ -260,7 +241,7 @@ int adminFunctions(){
         else
         {
             cout<<"\nYou Cannot Delete Yourself at this time!";
-            Sleep(4000);
+            pause_screen();
             adminFunctions();
         }
         
@@ -275,7 +256,7 @@ int adminFunctions(){
         if (acc)
         {
             cout<<"Member Already Exist!";
-            Sleep(3000);
+            pause_screen();
             adminFunctions();
         }
         else
@@ -288,7 +269,7 @@ int adminFunctions(){
             if (!creds)
             {
                 cout<<"Error Creating Account! Please try again!"<<endl;
-                Sleep(3000);
+                pause_screen();
                 adminFunctions();
             }
             else
@@ -300,7 +281,7 @@ int adminFunctions(){
                 creds <<  enc_un << endl << enc_up << endl;
                 cout<<"Registered Successfully!" << endl;
                 creds.close();
-                Sleep(3000);
+                pause_screen();
                 adminFunctions();
             }
         }
@@ -313,164 +294,197 @@ int adminFunctions(){
     {
         cout<<"\n\nInvalid Choice!";
         adminFunctions();
-    }
-    
-    
+    } 
 }
 
 
 bool checkRateLimit(){
     ifstream rl;
-    string rlTime;
-    if (!rl)
-    {
-        return false;
-    } else
+
+    #ifdef lx
+    rl.open("/tmp/limit");
+    #else
+    rl.open("limit");
+    #endif
+
+    if (rl)
     {
         return true;
+    } else
+    {
+        return false;
     }
-    
-    
 }
 
 int main(){
     if(ChCrDir()){
-        // string LoggedInUser;
-        clear_screen();
-        int choice;
-        // cout<<"menu:\n1.Register\n2.Login\n3.Exit"<<endl;
-        cout<<"-----------------------------------------------------------------------------------------------------------------\n";
-        cout<<"-----------------------------------------------------------------------------------------------------------------\n\n";
-        cout<<"                        advanced User Management System via File Handling and Classes\n\n";
-        cout<<"                                              Made by NEXUS\n";
-        cout<<"-----------------------------------------------------------------------------------------------------------------\n";
-        cout<<"-----------------------------------------------------------------------------------------------------------------\n\n";
-        cout<<"1] Register\n";
-        cout<<"2] Login\n";
-        cout<<"3] Exit\n\n";
-        cout<<"-----------------------------------------------------------------------------------------------------------------\n";
-        cout<<"Choice: ";
-        cin>>choice;
-
-        string username, password, un, up;
-        
-        if (choice==1){
+        if (!checkRateLimit())
+        {
             clear_screen();
-            cout<<"Enter Username To Register with: ";
-            cin>>username;
+            int choice;
+            cout<<"-----------------------------------------------------------------------------------------------------------------\n";
+            cout<<"-----------------------------------------------------------------------------------------------------------------\n\n";
+            cout<<"                        advanced User Management System via File Handling and Classes\n\n";
+            cout<<"                                              Made by NEXUS\n";
+            cout<<"-----------------------------------------------------------------------------------------------------------------\n";
+            cout<<"-----------------------------------------------------------------------------------------------------------------\n\n";
+            cout<<"1] Register\n";
+            cout<<"2] Login\n";
+            cout<<"3] Exit\n\n";
+            cout<<"-----------------------------------------------------------------------------------------------------------------\n";
+            cout<<"Choice: ";
+            cin>>choice;
 
-            ifstream acc;
-            acc.open("database\\"+username);
-            if (acc)
-            {
-                cout<<"Member Already Exist!";
-                main();
-            }
-            else
-            {
-                cout<<"Create a Password: ";
-                cin>>password;
+            string username, password, un, up;
+            
+            if (choice==1){
+                clear_screen();
+                cout<<"Enter Username To Register with: ";
+                cin>>username;
 
-                ofstream creds;
-                creds.open("database\\"+username);
-                if (!creds)
+                ifstream acc;
+                acc.open("database\\"+username);
+                if (acc)
                 {
-                    cout<<"Error Creating Account! Please try again!"<<endl;
+                    cout<<"Member Already Exist!";
+                    main();
                 }
                 else
                 {
-                    // int un_size = username.length()+1;
-                    // char cr_arr[un_size];
-                    string enc_un = enc_b64(username, username.length());
-                    string enc_up = enc_b64(password, password.length());
-                    creds <<  enc_un << endl << enc_up << endl;
-                    cout<<"Registered Successfully!" << endl;
-                    creds.close();
-                    main();
-                }
-            }
-        }
-
-        else if (choice==2)
-        {
-            clear_screen();
-            cout<<"Enter Username: ";
-            cin>>username;
-
-            ifstream locate;
-            locate.open("database\\"+username);
-            if (!locate)
-            {
-                cout<<"You are not an member, register first!";
-                exit(1);
-            }
-            else
-            {
-                int tempCounter=3;
-                do{
-                    cout<<"Enter Password for "<<username<<" : ";
+                    cout<<"Create a Password: ";
                     cin>>password;
-        
-                    ifstream user;
-                    user.open("database\\"+ username);
-                    getline(user, un);
-                    getline(user, up);
 
-                    // int un_size = un.length()+1;
-                    // int up_size = up.length()+1;
-
-                    if (username==dc_b64(un, un.length()) && password==dc_b64(up, up.length()))
+                    ofstream creds;
+                    creds.open("database\\"+username);
+                    if (!creds)
                     {
-                        cout<<"Successfully Logged in..!";
-                        LoggedInUser = username;
-                        if(LoggedInUser == ADMIN_USERNAME) {
-                            // break;
-                            adminFunctions();
-                            break;
-                        }else{
-                            exit(1);
-                        }
-
+                        cout<<"Error Creating Account! Please try again!"<<endl;
                     }
                     else
                     {
-                        cout<<"\npassword is wrong for username\n";
-                        // exit(1);
-                        tempCounter--;
+                        // int un_size = username.length()+1;
+                        // char cr_arr[un_size];
+                        string enc_un = enc_b64(username, username.length());
+                        string enc_up = enc_b64(password, password.length());
+                        creds <<  enc_un << endl << enc_up << endl;
+                        cout<<"Registered Successfully!" << endl;
+                        creds.close();
+                        pause_screen();
+                        main();
                     }
-                }while(tempCounter!=0);
-                cout<<"\nYou have exceeded Number of Password Tries. Your System is Rate-Limited for 10 minutes\n";
-                ofstream rl;
-                string rlTime = getLimitTime();
-                #ifdef lx
-                rl.open("/tmp/limited");
-                rl<<rlTime;
-                #else
-                rl.open("C:\\limited");
-                rl<<rlTime;
-                #endif
-
-                exit(1);
+                }
             }
+
+            else if (choice==2)
+            {
+                clear_screen();
+                cout<<"Enter Username: ";
+                cin>>username;
+
+                ifstream locate;
+                locate.open("database\\"+username);
+                if (!locate)
+                {
+                    cout<<"You are not an member, register first!";
+                    main();
+                }
+                else
+                {
+                    int tempCounter=3;
+                    do{
+                        cout<<"Enter Password for "<<username<<" : ";
+                        cin>>password;
             
-        } else if (choice==3)
-        {
-            clear_screen();
-            cout<<"Thanks for Using.\nExiting...";
-            exit(0);
+                        ifstream user;
+                        user.open("database\\"+ username);
+                        getline(user, un);
+                        getline(user, up);
+
+                        // int un_size = un.length()+1;
+                        // int up_size = up.length()+1;
+
+                        if (username==dc_b64(un, un.length()) && password==dc_b64(up, up.length()))
+                        {
+                            cout<<"Successfully Logged in..!";
+                            LoggedInUser = username;
+                            if(LoggedInUser == ADMIN_USERNAME) {
+                                // break;
+                                adminFunctions();
+                                break;
+                            }else{
+                                exit(1);
+                            }
+                        }
+                        else
+                        {
+                            cout<<"\npassword is wrong for username\n";
+                            // exit(1);
+                            tempCounter--;
+                        }
+                    }while(tempCounter!=0);
+                    cout<<"\nYou have exceeded Number of Password Tries. Your System is Rate-Limited for 10 minutes\n";
+                    ofstream rl;
+                    string rlTime = getLimitTime();
+                    #ifdef lx
+                    rl.open("/tmp/limit");
+                    rl<<rlTime;
+                    #else
+                    rl.open("limit");
+                    rl<<rlTime;
+                    rl.close();
+                    #endif
+                    exit(1);
+                }
+                
+            } else if (choice==3)
+            {
+                clear_screen();
+                cout<<"Thanks for Using.\nExiting...";
+                exit(0);
+            } else
+            {
+                cout<<"\n\nInvalid Choice!";
+                main();
+            }
         } else
         {
-            cout<<"\n\nInvalid Choice!";
-            main();
+            ifstream rlTime;
+            string limitTime;
+            #ifdef lx
+            rlTime.open("/tmp/limit");
+            #else
+            rlTime.open("limit");
+            getline(rlTime, limitTime);
+
+            std::tm time;
+            std::istringstream time_stream(limitTime);
+            time_stream >> std::get_time(&time, "%a %b %d %H:%M:%S %Y");
+
+            // Convert the time to a time point
+            auto stored_time = std::chrono::system_clock::from_time_t(std::mktime(&time));
+
+            // Get the current time
+            auto now = std::chrono::system_clock::now();
+
+            // Check if the current time is greater than the stored time
+            string dir_path="";
+            string limitFile="limit";
+            if (now > stored_time) {
+                cout<<"\n\ndeleting file!\n\n"<<endl;
+                DeleteFile((dir_path+limitFile).c_str());
+                main();
+            } else {
+                cout << "You Have still not Exceeded The Rate Limit.\nUnlock Time:" << limitTime << endl;
+                exit(1);
+            }
+            #endif          
         }
-        
-        
     }
     else
     {
-        cout<<"ughh... Some Totaly unexpected error occured .. man .. shit!";
+        cout<<"Some Totaly unexpected error occured.";
         exit(1);
     }
-    
+
     return 0;
 }
