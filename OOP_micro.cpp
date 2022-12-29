54dd2c471f4099f9f608e45418edf2b52d3aa198
Jeevan-Gharate <59790218+Jeevan-Gharate@users.noreply.github.com>
2022-12-29 15:24:00 +0530

    added username and password encoding

--- OOP_micro.cpp
+++ OOP_micro.cpp
@@ -1,8 +1,132 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#define SIZE 1000
+
 
 using namespace std;
+
+char* enc_b64(string input_str, int len_str)
+{
+   
+    char char_set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+      
+    char *res_str = (char *) malloc(SIZE * sizeof(char));
+      
+    int index, no_of_bits = 0, padding = 0, val = 0, count = 0, temp;
+    int i, j, k = 0;
+      
+    for (i = 0; i < len_str; i += 3)
+        {
+            val = 0, count = 0, no_of_bits = 0;
+  
+            for (j = i; j < len_str && j <= i + 2; j++)
+            {
+
+                val = val << 8;
+
+                val = val | input_str[j];
+
+                count++;
+              
+            }
+  
+            no_of_bits = count * 8;
+  
+            padding = no_of_bits % 3;
+  
+
+            while (no_of_bits != 0)
+            {
+                if (no_of_bits >= 6)
+                {
+                    temp = no_of_bits - 6;
+                      
+                    index = (val >> temp) & 63;
+                    no_of_bits -= 6;        
+                }
+                else
+                {
+                    temp = 6 - no_of_bits;
+                      
+                    index = (val << temp) & 63;
+                    no_of_bits = 0;
+                }
+                res_str[k++] = char_set[index];
+            }
+    }
+
+    for (i = 1; i <= padding; i++)
+    {
+        res_str[k++] = '=';
+    }
+  
+    res_str[k] = '\0';
+  
+    return res_str;
+  
+}
+
+
+char* dc_b64(string encoded, int len_str)
+{
+    char* decoded_string;
+ 
+    decoded_string = (char*)malloc(sizeof(char) * SIZE);
+ 
+    int i, j, k = 0;
+ 
+    int num = 0;
+ 
+
+    int count_bits = 0;
+ 
+    for (i = 0; i < len_str; i += 4)
+    {
+        num = 0, count_bits = 0;
+        for (j = 0; j < 4; j++)
+        {
+             
+            if (encoded[i + j] != '=')
+            {
+                num = num << 6;
+                count_bits += 6;
+            }
+ 
+            if (encoded[i + j] >= 'A' && encoded[i + j] <= 'Z')
+                num = num | (encoded[i + j] - 'A');
+
+            else if (encoded[i + j] >= 'a' && encoded[i + j] <= 'z')
+                num = num | (encoded[i + j] - 'a' + 26);
+ 
+            else if (encoded[i + j] >= '0' && encoded[i + j] <= '9')
+                num = num | (encoded[i + j] - '0' + 52);
+ 
+            else if (encoded[i + j] == '+')
+                num = num | 62;
+ 
+            else if (encoded[i + j] == '/')
+                num = num | 63;
+
+            else {
+                num = num >> 2;
+                count_bits -= 2;
+            }
+        }
+ 
+        while (count_bits != 0)
+        {
+            count_bits -= 8;
+
+            decoded_string[k++] = (num >> count_bits) & 255;
+        }
+    }
+
+    decoded_string[k] = '\0';
+ 
+    return decoded_string;
+}
+
 
 int main(){
     
@@ -11,7 +135,7 @@ int main(){
     cin>>choice;
 
     string username, password, un, up;
-
+    
     if (choice==1){
         cout<<"Enter Username To Register with: ";
         cin>>username;
@@ -36,7 +160,11 @@ int main(){
             }
             else
             {
-                creds << username << endl << password << endl;
+                // int un_size = username.length()+1;
+                // char cr_arr[un_size];
+                string enc_un = enc_b64(username, username.length());
+                string enc_up = enc_b64(password, password.length());
+                creds <<  enc_un << endl << enc_up << endl;
                 cout<<"Registered Successfully!" << endl;
                 creds.close();
                 main();
@@ -54,7 +182,7 @@ int main(){
         if (!locate)
         {
             cout<<"You are not an member, register first!";
-            main();
+            exit(1);
         }
         else
         {
@@ -65,14 +193,18 @@ int main(){
             user.open("database\\"+ username + ".txt");
             getline(user, un);
             getline(user, up);
-            if (un==username && up==password)
+
+            // int un_size = un.length()+1;
+            // int up_size = up.length()+1;
+
+            if (username==dc_b64(un, un.length()) && password==dc_b64(up, up.length()))
             {
                 cout<<"Successfully Logged in..!";
             }
             else
             {
                 cout<<"password is wrong for username";
-                main();
+                exit(1);
             }
             
         }
